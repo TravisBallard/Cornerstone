9d3ec645b8462ceac72ad095a453c01f16f830e0
Travis Ballard <travis.ballard@gmail.com>
2018-07-28 20:08:58 -0700

    Updates to make it work again.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,11 +2,11 @@
 
 angular.module('cornerstoneApp')
   .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
-    $scope.topics = $http.get('topics.json').success(function(r){
-      $scope.topics = r;
+    $scope.topics = $http.get('topics.json').then(function(r){
+      $scope.topics = r.data;
     });
 
-    $scope.workers = $http.get('workers.json').success(function(r){
-      $scope.workers = r;
+    $scope.workers = $http.get('workers.json').then(function(r){
+      $scope.workers = r.data;
     });
   }]);
--- app/scripts/directives/topic.js
+++ app/scripts/directives/topic.js
@@ -1,28 +1,28 @@
 'use strict';
 angular.module('cornerstoneApp')
-	.directive( 'topic', function(){
-		return {
-			restrict: 'A',
-			replace: true,
-			transclude: true,
-			link: function(scope, el){
-				angular.element(document).ready(function(){
+  .directive( 'topic', function(){
+    return {
+      restrict: 'A',
+      replace: true,
+      transclude: true,
+      link: function(scope, el){
+        angular.element(document).ready(function(){
 
-					var $ = window.$,
-						$el = $(el),
-						$icon = $(el).find('.icon');
+          var $ = window.$,
+            $el = $(el),
+            $icon = $(el).find('.icon');
 
-					$el.hover(function(){
-						if( $icon.hasClass('fadeInDownBig')){
-							$icon.removeClass('fadeInDownBig');
-						}
-						$icon.addClass('swing');
-					}, function(){
-						$icon.removeClass('swing');
-					});
+          $el.hover(function(){
+            if( $icon.hasClass('fadeInDownBig')){
+              $icon.removeClass('fadeInDownBig');
+            }
+            $icon.addClass('swing');
+          }, function(){
+            $icon.removeClass('swing');
+          });
 
-				});
-			},
-			templateUrl: 'views/topic.html'
-		};
-	});
+        });
+      },
+      templateUrl: 'views/topic.html'
+    };
+  });
